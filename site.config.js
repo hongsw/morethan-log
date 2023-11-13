c92641408a9e7beb4569d7aaa57384ed5690e2f6
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-11-13 20:32:18 +0900

    openai-actions-api-list

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `OpenAI Actions`,
+      href: "https://github.com/hongsw/openai-actions-api-list",
     },
   ],
   // blog setting (required)
