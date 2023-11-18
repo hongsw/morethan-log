ceaefe50e318ae54743fe7d0d57a2198051b8c28
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-11-19 00:03:01 +0900

    avatar변경

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "martinhong",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "GPT developer",
     bio: "I develop everything using GPT.",
     email: "hongbuzz_at_gmail.com",
